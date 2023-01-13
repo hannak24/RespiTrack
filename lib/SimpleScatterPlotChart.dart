7a375a3f190db57555401b53efd301ab28d52bde
hannak24 <73960535+hannak24@users.noreply.github.com>
2023-01-13 16:57:27 +0200

    all colors but missed added

--- lib/SimpleScatterPlotChart.dart
+++ lib/SimpleScatterPlotChart.dart
@@ -155,13 +155,16 @@ class _SimpleScatterPlotChartDBState extends State<SimpleScatterPlotChartDB> {
                 var secondAlarm = "18:00:00";
                 if(snapshot2.hasData){
                   DocumentSnapshot documentSnapshot = snapshot2.data?.docs[0];
-                  firstAlarm =  snapshot2.data?.docs[0]["time"].split(":");
-                  secondAlarm = snapshot2.data?.docs[1]["time"].split(":");
+                  firstAlarm =  snapshot2.data?.docs[0]["time"];
+                  secondAlarm = snapshot2.data?.docs[1]["time"];
                 }
                 Widget widget = Container();
                 List<charts.Series<medicineIntake, int>> chartData = <charts.Series<medicineIntake, int>>[];
                 if (snapshot1.hasData) {
                   List<medicineIntake> onTime = <medicineIntake>[];
+                  List<medicineIntake> late = <medicineIntake>[];
+                  List<medicineIntake> veryLate = <medicineIntake>[];
+                  List<medicineIntake> missed = <medicineIntake>[];
                   for (int index = 0; index < snapshot1.data?.docs.length; index++) {
                     DocumentSnapshot documentSnapshot = snapshot1.data?.docs[index];
                     var initialTime = documentSnapshot["dateTime"].replaceAll(".","-");
@@ -171,20 +174,108 @@ class _SimpleScatterPlotChartDBState extends State<SimpleScatterPlotChartDB> {
                     var fixedTime = fixedDate +" "+ temp[0]+"Z";
                     DateTime pushTime = DateTime.parse(fixedTime);
 
-                    medicineIntake intakeTime = medicineIntake(pushTime,"onTime");
-                    onTime.add(intakeTime);
+                    var fixedAlarm1 = fixedDate +" "+ firstAlarm+"Z";
+                    var fixedAlarm2 = fixedDate +" "+ secondAlarm+"Z";
+                    DateTime alarm1Time = DateTime.parse(fixedAlarm1);
+                    DateTime alarm2Time = DateTime.parse(fixedAlarm2);
+                    var closestAlarm =
+                    (pushTime.isAfter(alarm1Time) || pushTime.isAtSameMomentAs(alarm1Time)) && pushTime.isBefore(alarm2Time)?
+                    alarm1Time: alarm2Time;
+
+                    var farAlarm =
+                    (pushTime.isAfter(alarm1Time) || pushTime.isAtSameMomentAs(alarm1Time)) && pushTime.isBefore(alarm2Time)?
+                    alarm2Time: alarm1Time;
+                    var status = "missed";
+
+                    print(closestAlarm);
+                    print(farAlarm);
+
+                    //checking status
+
+                    print("pushTime: ");
+                    print(pushTime);
+                    print("closestAlarm: ");
+                    print(closestAlarm);
+                    print(pushTime.hour-closestAlarm.hour);
+                    print(pushTime.minute - closestAlarm.minute);
+                    print(pushTime.hour == closestAlarm.hour);
+                    print(pushTime.minute - closestAlarm.minute < 11);
+
+                    if((pushTime.hour == closestAlarm.hour && pushTime.minute - alarm1Time.minute > 11)
+                        || pushTime.hour - closestAlarm.hour < 3){
+                      status = "late";
+                      medicineIntake intakeTime = medicineIntake(pushTime,status);
+                      late.add(intakeTime);
+                    }
+
+                    if((pushTime.hour == closestAlarm.hour) && (pushTime.minute - closestAlarm.minute < 11)){
+                        status = "onTime";
+                        medicineIntake intakeTime = medicineIntake(pushTime,status);
+                        onTime.add(intakeTime);
+                        print("I'm here");
+                    }
+
+
+
+
+                    if(pushTime.hour - closestAlarm.hour > 3 && pushTime.isBefore(farAlarm)){
+                      status = "veryLate";
+                      medicineIntake intakeTime = medicineIntake(pushTime,status);
+                      veryLate.add(intakeTime);
+                    }
+
                   }
-                  chartData = [new charts.Series<medicineIntake, int>(
-                    id: 'In time',
-                    // Providing a color function is optional.
-                    colorFn: (medicineIntake medicineTime, _) {
-                      return charts.MaterialPalette.blue.shadeDefault;
-                    },
-                    domainFn: (medicineIntake times, _) => times.dateTime.day,
-                    measureFn: (medicineIntake times, _) => times.dateTime.hour,
-                    // // Providing a radius function is optional.
-                    data: onTime,
-                  )];
+                  print("onTime");
+                  print(onTime);
+                  chartData = [
+
+                new charts.Series<medicineIntake, int>(
+                      id: 'Late',
+                      // Providing a color function is optional.
+                      colorFn: (medicineIntake medicineTime, _) {
+                        return charts.MaterialPalette.lime.shadeDefault;
+                      },
+                      domainFn: (medicineIntake times, _) => times.dateTime.day,
+                      measureFn: (medicineIntake times, _) => times.dateTime.hour,
+                      // // Providing a radius function is optional.
+                      data: late,
+                    ),
+                new charts.Series<medicineIntake, int>(
+                id: 'In time',
+                // Providing a color function is optional.
+                colorFn: (medicineIntake medicineTime, _) {
+                return charts.MaterialPalette.blue.shadeDefault;
+                },
+                domainFn: (medicineIntake times, _) => times.dateTime.day,
+                measureFn: (medicineIntake times, _) => times.dateTime.hour,
+                // // Providing a radius function is optional.
+                data: onTime,
+                ),
+
+                new charts.Series<medicineIntake, int>(
+                id: 'Very late',
+                // Providing a color function is optional.
+                colorFn: (medicineIntake medicineTime, _) {
+                return charts.MaterialPalette.red.shadeDefault;
+                },
+                domainFn: (medicineIntake times, _) => times.dateTime.day,
+                measureFn: (medicineIntake times, _) => times.dateTime.hour,
+                // // Providing a radius function is optional.
+                data: veryLate,
+                ),
+
+
+                new charts.Series<medicineIntake, int>(
+                id: 'Missed',
+                // Providing a color function is optional.
+                colorFn: (medicineIntake medicineTime, _) {
+                return charts.MaterialPalette.black;
+                },
+                domainFn: (medicineIntake times, _) => times.dateTime.day,
+                measureFn: (medicineIntake times, _) => times.dateTime.hour,
+                // // Providing a radius function is optional.
+                data: missed,
+                )];
                   widget = new charts.ScatterPlotChart(
                     chartData,
                     animate: true,
